bbe146c7e860d3031dffbee12c0442726f3b4f80
gramsey1 <graceramsey312@yahoo.com>
2018-09-11 11:23:11 -0400

    later

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("Would you like to add, subtract, multiply or divide?");
+        String choose = kbReader.nextLine();
+        Boolean add = true;
+        int ad = 4 + 3;
+        if (add) {
+            System.out.println("What numbers do you want to add?");
+            String numbers = kbReader.nextLine();
+            double d = numbers
+                    double sqrt
+        }
+            System.out.println("The sum of  Are you satisfied?");
+            String satisfied = kbReader.nextLine();
+            Boolean no = true;
+            if (no) {
+                System.out.println("Can you identify what math class method I ran the real answer through?");
+                String identify = kbReader.nextLine();
+                Boolean sqrt = true;
+                if (sqrt) {
+                    System.out.println("Great! Good job and goodbye!");
+                }
+            }
+        }
     }
 }
